531262a60d3c756e350ed5b10dc88e872a432615
Abhimanyu V <91383581+abhiselvi@users.noreply.github.com>
2023-01-25 11:33:03 +0530

    Add files via upload

---
+++ File_Org.py
@@ -0,0 +1,132 @@
+#!/bin/python3
+import glob
+import os
+import sys
+import re
+import mimetypes
+import time
+import shutil
+import platform
+
+system = platform.platform()
+if 'Win' in system:
+    filedelimiter = "\\"
+else:
+    filedelimiter = "/"
+
+# glob, glob, glob if pred do action
+def move(f, t, doPrompt = False):
+    if doPrompt:
+        if not prompt(f):
+            return
+    os.renames(f, t)
+
+def copy(f, t, doPrompt = False):
+    if doPrompt:
+        if not prompt(f):
+            return
+    pathEntities = t.split(filedelimiter)
+    parent = filedelimiter.join(pathEntities[0:len(pathEntities) - 1])
+    try:
+        os.makedirs(parent)
+    except FileExistsError:
+        pass
+    shutil.copy(f, t)
+
+def delete(f, doPrompt = False):
+    if doPrompt:
+        if not prompt(f):
+            return
+    os.remove(f)
+
+def prompt(f):
+    print(f + ": Are you sure? (y/n)")
+    reply = input().lower()
+    if reply == 'y':
+        return True
+    else:
+        return False
+
+# returns pattern, cond, action
+def parse(source):
+    if 'IF' not in source:
+        source = source.replace('DO', ' IF DO');
+    parts = list(re.findall('(.*)IF(.*)DO(.*)', source)[0])
+    for i in range(3):
+        parts[i] = parts[i].strip()
+    return parts
+
+folder = os.getcwd()
+def cmd(line):
+    global folder
+    # change directory
+    if 'DO' not in line:
+        os.chdir(line)
+        folder = os.getcwd()
+        return
+    # parsing
+    pattern, predicate, action = parse(line)
+    if pattern == '':
+        pattern = '*'
+    pattern = folder + filedelimiter + pattern
+    if predicate == '':
+        predicate = 'True'
+    if action == '':
+        action = 'print(path)'
+    print(predicate)
+    # macros
+    predicate = predicate.replace('KB', '* 1000')
+    predicate = predicate.replace('MB', '* 1000000')
+    predicate = predicate.replace('GB', '* 1000000000')
+    predicate = predicate.replace('MIN', '* 60')
+    predicate = predicate.replace('HR', '* 3600')
+    predicate = predicate.replace('DAY', '* 86400')
+    action = re.sub('^alert\s+delete$', 'delete(path, True)', action)
+    action = re.sub('^delete$', 'delete(path)', action)
+    action = re.sub('^alert\s+move\s+to\s+(.*)$', 'move(path, \'\\1\' + filedelimiter + name, True)', action)
+    action = re.sub('^move\s+to\s+(.*)$', 'move(path, \'\\1\' + filedelimiter + name)', action)
+    action = re.sub('^alert\s+copy\s+to\s+(.*)$', 'copy(path, \'\\1\' + filedelimiter + name, True)', action)
+    action = re.sub('^copy\s+to\s+(.*)$', 'copy(path, \'\\1\' + filedelimiter + name)', action)
+    # actions!
+    global path
+    #print(predicate)
+    for path in glob.glob(pattern, recursive = True):
+        if os.path.isfile(path):
+            #print(path)
+            stat = os.stat(path)
+            global size
+            size = stat.st_size # in bytes
+            global name
+            name = path.split(filedelimiter)[-1]
+            global parent
+            parent = path.split(filedelimiter)[-2]
+            global extension
+            extension = path.split('.')[-1]
+            global mimetype
+            mimetype = mimetypes.guess_type(path)[0]
+            if mimetype == None:
+                mimetype = ''
+            global accessed
+            accessed = time.time() - stat.st_atime
+            global modified
+            modified = time.time() - stat.st_mtime
+            if eval(predicate, globals()):
+                print('PROCESSING ', path)
+                exec(action)
+
+# MAIN
+if len(sys.argv) < 2:
+    command = ''
+    while(command != 'quit'):
+            if command != '':
+                cmd(command)
+            print('\x1b[1;33m' + os.getcwd() + '> ', end='');
+            print('\x1b[0;33m', end='');
+            command = input().strip();
+            print('\x1b[0m', end='');
+else:
+    with open(sys.argv[1]) as f:
+        for line in f:
+            cmd(line)
+
+
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# Perfectionist
-A source for accessing our files in file manager.
+An automatic file organizer which organizes the files according to the rules set by the user.
+
+# Examples
+```
+Delete all compiled .class files
+/home/crayonie/*.class DO delete(path)
+Move all c files to c_programs directory
+/home/crayonie/*.c DO move(path, '/home/crayonie/c_programs/' + name)
+Prompt and delete the files bigger than 1 GB
+/home/crayonie/* IF size > 1 GB DO delete(path, True)
+Copy the files accessed before half an hour to recents folder
+/home/crayonie/* IF accessed < 30 MIN DO copy(path, '/home/crayonie/recents/' + name)
+Copy the files modified before 12 hours to recents folder
+/home/crayonie/* IF modified < 12 HR DO copy(path, '/home/crayonie/recents/' + name)
+```
+
+# SYNTAX
+pattern IF condition DO action
+
+# Features
+ - Predicates
+   - path
+   - size
+   - name
+   - parent
+   - folder (current working directory)
+   - extension
+   - mimetype
+   - accessed (time elapsed from latest access)
+   - modified (time elapsed from latest modification)
+ - Actions
+   - Move
+   - Copy
+   - Delete
+ - Script mode and command mode
+
+# Feature Roadmap
+## POC 2
+ - Help menu
+ - Better cui support
+
+ - Rewrite whole shit
+ - Error handling
+ - Safe moving
+ - Tagging system
+ - Handle links and folders
+ - Full text searching
+ - Enhanced datetime support (with today)
+ - Duplicate detection
+ - Bulk Rename
